51be3e58adeb58e9bca635851198e34399fb14c3
Brian Sayler <saylerb@users.noreply.github.com>
2025-05-19 10:07:19 -0600

    added neotest as a way to run go tests within vim

--- init.lua
+++ init.lua
@@ -239,6 +239,102 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'fredrikaverpil/neotest-golang', -- Installation
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-golang', -- Registration
+        },
+      }
+    end,
+    keys = {
+      {
+        '<leader>ta',
+        function()
+          require('neotest').run.attach()
+        end,
+        desc = '[t]est [a]ttach',
+      },
+      {
+        '<leader>tf',
+        function()
+          require('neotest').run.run(vim.fn.expand '%')
+        end,
+        desc = '[t]est run [f]ile',
+      },
+      {
+        '<leader>tA',
+        function()
+          require('neotest').run.run(vim.uv.cwd())
+        end,
+        desc = '[t]est [A]ll files',
+      },
+      {
+        '<leader>tS',
+        function()
+          require('neotest').run.run { suite = true }
+        end,
+        desc = '[t]est [S]uite',
+      },
+      {
+        '<leader>tn',
+        function()
+          require('neotest').run.run()
+        end,
+        desc = '[t]est [n]earest',
+      },
+      {
+        '<leader>tl',
+        function()
+          require('neotest').run.run_last()
+        end,
+        desc = '[t]est [l]ast',
+      },
+      {
+        '<leader>ts',
+        function()
+          require('neotest').summary.toggle()
+        end,
+        desc = '[t]est [s]ummary',
+      },
+      {
+        '<leader>to',
+        function()
+          require('neotest').output.open { enter = true, auto_close = true }
+        end,
+        desc = '[t]est [o]utput',
+      },
+      {
+        '<leader>tO',
+        function()
+          require('neotest').output_panel.toggle()
+        end,
+        desc = '[t]est [O]utput panel',
+      },
+      {
+        '<leader>tt',
+        function()
+          require('neotest').run.stop()
+        end,
+        desc = '[t]est [t]erminate',
+      },
+      {
+        '<leader>td',
+        function()
+          require('neotest').run.run { suite = false, strategy = 'dap' }
+        end,
+        desc = 'Debug nearest test',
+      },
+    },
+  },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
