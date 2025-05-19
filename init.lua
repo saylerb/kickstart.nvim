1f5e4631b1fda4758bd0070e7a85881613aa5d50
Brian Sayler <saylerb@users.noreply.github.com>
2025-05-19 10:08:43 -0600

    tried out nvim-test and gopher.nvim but did not end up going with them

--- init.lua
+++ init.lua
@@ -336,6 +336,51 @@ require('lazy').setup({
     },
   },
 
+  -- {
+  --   'klen/nvim-test',
+  --   config = function()
+  --     require('nvim-test').setup {}
+  --   end,
+  -- },
+  -- {
+  --   'olexsmir/gopher.nvim',
+  --   ft = 'go',
+  --   -- branch = "develop", -- if you want develop branch
+  --   -- keep in mind, it might break everything
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'mfussenegger/nvim-dap', -- (optional) only if you use `gopher.dap`
+  --   },
+  --   -- (optional) will update plugin's deps on every update
+  --   build = function()
+  --     vim.cmd.GoInstallDeps()
+  --   end,
+  --   ---@type gopher.Config
+  --   opts = {
+  --     commands = {
+  --       go = 'go',
+  --       gomodifytags = 'gomodifytags',
+  --       gotests = 'gotests',
+  --       impl = 'impl',
+  --       iferr = 'iferr',
+  --       dlv = 'dlv',
+  --     },
+  --     gotests = {
+  --       -- gotests doesn't have template named "default" so this plugin uses "default" to set the default template
+  --       template = 'default',
+  --       -- path to a directory containing custom test code templates
+  --       template_dir = nil,
+  --       -- switch table tests from using slice to map (with test name for the key)
+  --       -- works only with gotests installed from develop branch
+  --       named = false,
+  --     },
+  --     gotag = {
+  --       transform = 'snakecase',
+  --     },
+  --   },
+  -- },
+  --
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
